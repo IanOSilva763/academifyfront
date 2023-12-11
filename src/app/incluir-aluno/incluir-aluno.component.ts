dcf8bac9829b30f9ff05320c0fe87f45156f0737
IanOSilva763 <ian.o.s@hotmail.com>
2023-12-11 02:09:57 -0300

    vizualizar

--- src/app/incluir-aluno/incluir-aluno.component.ts
+++ src/app/incluir-aluno/incluir-aluno.component.ts
@@ -35,27 +35,16 @@ export class IncluirAlunoComponent implements OnInit {
   salvarAluno() {
     if (this.alunoForm.valid) {
       const alunoData = this.alunoForm.value;
-<<<<<<< HEAD
   
       // Formatar a data para o formato 'yyyy-MM-dd'
       const dataNascimentoFormatada = this.formatarData(alunoData.nascimento);
       alunoData.nascimento = dataNascimentoFormatada;
   
-=======
-
-      // Formatar a data para o formato 'yyyy-MM-dd'
-      const dataNascimentoFormatada = this.formatarData(alunoData.nascimento);
-      alunoData.nascimento = dataNascimentoFormatada;
-
->>>>>>> 6e75900e8997c037c94771f127cc77cf66ff06c0
       this.alunoService.criarAluno(alunoData).subscribe(
         (response: any) => {
           console.log('Aluno salvo com sucesso:', response);
           this.cadastroSucesso = true;
-<<<<<<< HEAD
           this.exibirDialogo(); // Chama o método para exibir o snackbar e redirecionar
-=======
->>>>>>> 6e75900e8997c037c94771f127cc77cf66ff06c0
         },
         (error: any) => {
           console.error('Erro ao salvar aluno:', error);
@@ -63,10 +52,7 @@ export class IncluirAlunoComponent implements OnInit {
       );
     }
   }
-<<<<<<< HEAD
   
-=======
->>>>>>> 6e75900e8997c037c94771f127cc77cf66ff06c0
 
   // Função para formatar a data para 'yyyy-MM-dd'
   formatarData(data: string): string {
@@ -74,27 +60,12 @@ export class IncluirAlunoComponent implements OnInit {
     return `${partes[2]}-${partes[1]}-${partes[0]}`;
   }
 
-<<<<<<< HEAD
-=======
-  fecharMensagem() {
-    this.cadastroSucesso = false;
-  }
-
->>>>>>> 6e75900e8997c037c94771f127cc77cf66ff06c0
   exibirDialogo(): void {
     const mensagem = 'Aluno cadastrado com sucesso!';
     this.snackBar.open(mensagem, 'OK', {
       duration: 2000, // Tempo de exibição em milissegundos (opcional)
-<<<<<<< HEAD
     }).afterDismissed().subscribe(() => {
       this.router.navigate(['list']); // Redireciona para a página de listagem após o fechamento do snackbar
-=======
-    });
-
-    // Redirecione para a página list após o fechamento do snackbar
-    this.snackBar._openedSnackBarRef?.onAction().subscribe(() => {
-      this.router.navigate(['list']);
->>>>>>> 6e75900e8997c037c94771f127cc77cf66ff06c0
     });
   }
 }
